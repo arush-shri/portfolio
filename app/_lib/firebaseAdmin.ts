6f186a73efec4c0f71146f6e36eaaef6f32b3504
arush-shri <arush.sdl.shrivastava@gmail.com>
2026-02-21 22:48:00 +0530

    env fixes

--- app/_lib/firebaseAdmin.ts
+++ app/_lib/firebaseAdmin.ts
@@ -1,17 +1,4 @@
 import { App, cert, getApps, initializeApp } from "firebase-admin/app";
-
-const firebaseConfig = {
-	apiKey: "AIzaSyCuNBuSa5Q2Fz_Gp6-bJMTVIC3XeWNpfyw",
-	authDomain: "portfolio-arush.firebaseapp.com",
-	databaseURL:
-		"https://portfolio-arush-default-rtdb.asia-southeast1.firebasedatabase.app",
-	projectId: "portfolio-arush",
-	storageBucket: "portfolio-arush.firebasestorage.app",
-	messagingSenderId: "179526405783",
-	appId: "1:179526405783:web:c5e1598bf763adc6d3e3d2",
-	measurementId: "G-JRF3H6M92F",
-};
-
 // Initialize Firebase
 export const firebaseApp: App =
 	getApps().length === 0
